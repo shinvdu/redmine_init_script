418887403b0d3cfc47fbbbad75111df4ed16e6ca
tian <tian@tian-GA-MA785GT-UD3H.(none)>
2014-07-13 10:10:42 +0800

    https://gist.github.com/letsspeak/3981772

--- unicorn.rb
+++ unicorn.rb
@@ -1,26 +1,33 @@
-# Set the working application directory
-# # working_directory "/path/to/your/app"
-working_directory "/mnt/app/redmine"
-#
-# # Unicorn PID file location
-# # pid "/path/to/pids/unicorn.pid"
-pid "/mnt/app/redmine/pids/unicorn.pid"
-
-user "redmine", "redmine"
-#
-# # Path to logs
-# # stderr_path "/path/to/log/unicorn.log"
-# # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/mnt/app/redmine/log/unicorn.log"
-stdout_path "/mnt/app/redmine/log/unicorn.log"
-#
-# # Unicorn socket
-listen "/tmp/unicorn.redmine.sock"
-listen "/tmp/unicorn.redmine.sock"
-#
-# # Number of processes
-# # worker_processes 4
 worker_processes 2
-#
-# # Time-out
-timeout 30
+working_directory /mnt/app/redmine
+ 
+listen File.expand_path("tmp/unicorn.sock", ENV['RAILS_ROOT'])
+pid File.expand_path("tmp/unicorn.pid", ENV['RAILS_ROOT'])
+ 
+timeout 60
+ 
+preload_app true # ダウンタイムをなくす
+ 
+stdout_path File.expand_path("log/unicorn.stdout.log", ENV['RAILS_ROOT'])
+stderr_path File.expand_path("log/unicorn.stderr.log", ENV['RAILS_ROOT'])
+ 
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+ 
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+ 
+  old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        Process.kill(sig, File.read(old_pid).to_i)
+      rescue Errno::ENOENT, Errno::ESRCH
+      end 
+    end 
+ 
+    sleep 1
+  end 
+ 
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end

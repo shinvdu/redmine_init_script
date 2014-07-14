9b9d6853e8e4a6e50e63e21c10a7d001df771e55
root <root@li407-44.members.linode.com>
2014-07-14 14:52:42 +0000

    正常工作的第三个版本

--- unicorn.rb
+++ unicorn.rb
@@ -11,14 +11,14 @@ user "nginx", "nginx"
 # # Path to logs
 # # stderr_path "/path/to/log/unicorn.log"
 # # stdout_path "/path/to/log/unicorn.log"
-stderr_path "/mnt/app/redmine/log/unicorn.log"
-stdout_path "/mnt/app/redmine/log/unicorn.log"
+stderr_path "/mnt/app/redmine/log/unicorn.stderr.log"
+stdout_path "/mnt/app/redmine/log/unicorn.stdout.log"
 #
 # # Unicorn socket
 listen 9002
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "tmp/sockets/redmine.sock", :backlog => 64
+# listen "/mnt/app/redmine/tmp/sockets/redmine.sock", :backlog => 64
 # listen 8080, :tcp_nopush => true
 #
 # # Number of processes
